5edd753047fe699a8d5b11232c2917e94b39904d
Arnold Tsatsu <tsatsuarnold@gmail.com>
2024-04-26 22:36:59 +0000

    add env-risk

--- src/helpers/env-service.ts
+++ src/helpers/env-service.ts
@@ -1,7 +1,7 @@
 import * as yup from "yup";
 
 export const envService = {
-  serverUrl: "http://localhost:9006/api",
+  serverUrl: "http://34.125.56.76:3000/api",
   googleClientId: "742142303917-aob02ke2a6qja9o8hjcmqhb5kek8aaft.apps.googleusercontent.com",
 };
 
